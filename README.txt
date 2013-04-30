789035096f4965a899882917059205d32973a4aa
hilaryjoyce <hilary.paulin@gmail.com>
2013-04-30 15:08:10 -0400

    initial project version

---
+++ README.txt
@@ -0,0 +1,3 @@
+README
+
+This is a practice project to get used to using git!
---
+++ pytest.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a = np.arange(-100,101)
+plt.plot(a, a**2)
+plt.show()
